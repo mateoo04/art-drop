bf7e9554ad25e2ac05605b4bcedda1ed6dfc63ca
mateobubalo <mateobubalo04@gmail.com>
2026-05-13 16:10:45 +0200

    ci: fix profile used

--- ArtDrop/src/test/java/hr/tvz/artdrop/artdropapp/ArtDropApplicationTests.java
+++ ArtDrop/src/test/java/hr/tvz/artdrop/artdropapp/ArtDropApplicationTests.java
@@ -3,8 +3,10 @@ package hr.tvz.artdrop.artdropapp;
 import hr.tvz.artdrop.artdropapp.support.AbstractPostgresIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ArtDropApplicationTests extends AbstractPostgresIntegrationTest {
 
     @Test
